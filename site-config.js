4c2fd14b51bd88eac193f08807ed0ce399108cee
Developer <developer@example.com>
2026-09-15 00:17:07 +0500

    Connect frontend to Render backend

--- site-config.js
+++ site-config.js
@@ -1,1 +1,1 @@
-window.LEATHERCULTURE_API_BASE = window.LEATHERCULTURE_API_BASE || "";
+window.LEATHERCULTURE_API_BASE = window.LEATHERCULTURE_API_BASE || "https://leatherculture-backend.onrender.com";
